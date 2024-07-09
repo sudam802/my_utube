d1b377611d1265afbe3820dae5fdfbc0fd72267c
sudam802 <57206830+sudam802@users.noreply.github.com>
2024-07-09 10:37:17 +0530

    Update SidePannel.js

--- src/Components/SidePannel.js
+++ src/Components/SidePannel.js
@@ -4,19 +4,19 @@ import React from "react";
 const SidePanel = () => {
   return (
     <div class="ui visible thin inverted left vertical sidebar menu">
-      <a class="item">
-        <i class="home icon"></i>
+      <a class="item" href="">
+        <i class="home icon" href=""></i>
         Home
       </a>
-      <a class="item">
+      <a class="item" href="">
         <i class="block layout icon"></i>
         Topics
       </a>
-      <a class="item">
+      <a class="item" href="">
         <i class="smile icon"></i>
         Friends
       </a>
-      <a class="item">
+      <a class="item" href="">
         <i class="calendar icon"></i>
         History
       </a>
